df14ab34dec9f830f264a935d838ef83c31b1d83
Federico Figueroa C. <figueroa.development@gmail.com>
2026-09-11 17:03:23 -0600

    Update build process

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ app.get('/', (c) =>
 		<meta charset="UTF-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Live Audio Simulator</title>
-		${import.meta.env.PROD ? '<link rel="stylesheet" href="/static/assets/styles.css" />' : ''}
+		${import.meta.env.PROD ? '<link rel="stylesheet" href="/static/assets/main.css" />' : ''}
 	</head>
 	<body>
 		<div id="root"></div>
