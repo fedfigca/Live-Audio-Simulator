fa80e4777ee8a6eaffc0485852bfd3d3ec7c6ea9
Federico Figueroa C. <figueroa.development@gmail.com>
2026-09-11 16:25:34 -0600

    Refactors to Vercel / Vite / Hono integrations

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
-import build from '@hono/vite-build/node'
+import build from '@hono/vite-build/vercel'
 import devServer from '@hono/vite-dev-server'
 import react from '@vitejs/plugin-react'
 
